e4262e76d59ea6b71cb6f321c537d9895e6cf802
Barinovv <p.toxa@bk.ru>
2017-11-13 15:03:08 +0300

    Произведен рефакторинг. Удалено повторение кода. Добавлен комментарий.

--- src/ru/bav/catchup/Catchup.java
+++ src/ru/bav/catchup/Catchup.java
@@ -1,38 +1,43 @@
 package ru.bav.catchup;
 
+/**
+ * Класс, в котором выполяняются два потокп и меняется их приоритет.
+ *
+ * @author Barinov Anton 15ИТ18
+ */
+public class Catchup extends Thread {
 
-public class Catchup extends Thread {   
+    private String name;
+    private int firstPriority;
+    private int secondPriority;
+
+    public Catchup(String name, int firstPriority, int secondPriority) {
+        this.name = name;
+        this.firstPriority = firstPriority;
+        this.secondPriority = secondPriority;
+    }
+
     public void run() {
-        setPriority(MIN_PRIORITY);
+        setPriority(firstPriority);
         for (int i = 0; i < 4000; i++) {
             try {
                 sleep(1);
             } catch (InterruptedException e) {
             }
-            System.out.println(i + "." + "ДОГНАЛ");
+            System.out.println(i + "." + name);
             if (i == 550) {
-                setPriority(MAX_PRIORITY);
+                setPriority(secondPriority);
             }
 
         }
     }
 
     public static void main(String[] args) {
-        Catchup potok = new Catchup();
-        Thread thread1 = new Thread();
-        potok.start();
-        potok.setPriority(MAX_PRIORITY);
-        for (int i = 0; i < 4000; i++) {
-            try {
-                thread1.sleep(1);
-            } catch (InterruptedException e) {
-            }
-            System.out.println(i + "." + "Не догнал");
-            if (i == 500) {
-                thread1.setPriority(MIN_PRIORITY);
-            }
+        Catchup potok1 = new Catchup("Flash", MIN_PRIORITY, MAX_PRIORITY);
+        Catchup potok2 = new Catchup("Arrow", MAX_PRIORITY, MIN_PRIORITY);
+        potok1.start();
+        potok2.start();
 
-        }
     }
 }
 
