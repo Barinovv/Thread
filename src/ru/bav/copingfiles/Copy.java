52e0b28b50148dc43a95f86eb3e4f106062176bc
Barinovv <p.toxa@bk.ru>
2017-11-24 13:54:08 +0300

    Класс для копирования и вставки текста из файла в файл.

---
+++ src/ru/bav/copingfiles/Copy.java
@@ -0,0 +1,37 @@
+package ru.bav.copingfiles;
+
+import java.io.*;
+
+/*
+Класс
+ */
+
+public class Copy extends Thread {
+    private String adress;
+    private String result;
+
+    public Copy(String adress, String result) {
+        this.adress = adress;
+        this.result = result;
+    }
+
+
+
+    public void run(){
+        String string;
+        long before = System.currentTimeMillis();
+        try (BufferedReader read = new BufferedReader(new FileReader(adress));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(result))) {
+            while ((string = read.readLine()) != null){
+                writer.write(string + "\n");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } {
+
+        }
+        System.out.println((System.currentTimeMillis() - before) + " милесекунд(ы)");
+    }
+}
