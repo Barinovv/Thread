ce5c9d078fefa1ed5bcbb36a8afd5889ce380734
Barinovv <p.toxa@bk.ru>
2017-11-24 11:17:28 +0300

    Класс для запуска потоков.

---
+++ src/ru/bav/interference/InterferenceExample.java
@@ -0,0 +1,30 @@
+package ru.bav.interference;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Класс, в котором создаются и запускаются два потока для инкремента переменной i 1000 раз каждым потоком.
+ *
+ * @author Barinov Anton 1518
+ */
+
+public class InterferenceExample {
+    public static final int TWO_THOUSANDS = 2_000;
+    private AtomicInteger counter = new AtomicInteger();
+
+    public boolean stop() {
+        return counter.incrementAndGet() > TWO_THOUSANDS;
+    }
+
+    public void example() throws InterruptedException {
+        InterferenceThread thread1 = new InterferenceThread("Поток 1 - ", this);
+        InterferenceThread thread2 = new InterferenceThread("Поток 2 - ", this);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println("Ожидаем: " + TWO_THOUSANDS);
+        System.out.println("Получили: " + thread1.getI());
+    }
+}
+
