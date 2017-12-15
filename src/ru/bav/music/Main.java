631efd801da71abfd783015c04c973c9513a1477
Barinovv <p.toxa@bk.ru>
2017-12-15 12:30:15 +0300

    Добавлен класс main

---
+++ src/ru/bav/music/Main.java
@@ -0,0 +1,89 @@
+package ru.bav.music;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * Класс, в котором реализованно скачивание музыки.
+ *
+ * @author Barinov 1518
+ */
+
+public class Main {
+    private static final String IN_FILE_TXT = "C:\\Users\\User\\IdeaProjects\\Thread\\src\\ru\\bav\\music\\inFile.txt";
+    private static final String OUT_FILE_TXT = "C:\\Users\\User\\IdeaProjects\\Thread\\src\\ru\\bav\\music\\outFile.txt";
+    private static final String PATH_TO_MUSIC = "C:\\Users\\User\\IdeaProjects\\Thread\\src\\ru\\bav\\music\\";
+
+    public static void main(String[] args) {
+        first();
+        second();
+    }
+
+
+    private static void first() {
+        String Url;
+        try (BufferedReader inFile = new BufferedReader(new FileReader(IN_FILE_TXT));
+             BufferedWriter outFile = new BufferedWriter(new FileWriter(OUT_FILE_TXT))) {
+            while ((Url = inFile.readLine()) != null) {
+                URL url = new URL(Url);
+
+                String result;
+                try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()))) {
+                    result = bufferedReader.lines().collect(Collectors.joining("\n"));
+                }
+                Pattern email_pattern = Pattern.compile("\\s*(?<=data-url\\s?=\\s?\")[^>]*/*(?=\")");
+                Matcher matcher = email_pattern.matcher(result);
+                int i = 0;
+                while (matcher.find() && i < 2) {
+                    outFile.write(matcher.group() + "\r\n");
+                    i++;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static void second() {
+        try (BufferedReader musicFile = new BufferedReader(new FileReader(OUT_FILE_TXT))) {
+            String music;
+            int count = 0;
+            try {
+                while ((music = musicFile.readLine()) != null) {
+                    downloadUsingNIO(music, PATH_TO_MUSIC + String.valueOf(count) + ".mp3");
+                    count++;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    /**
+     * Метод, который осуществляет скачивание музыки
+     *
+     * @param strUrl для скачивания музыки
+     * @param file   наименование скачанного файла
+     * @throws IOException исключения
+     */
+
+    private static void downloadUsingNIO(String strUrl, String file) throws IOException {
+        URL url = new URL(strUrl);
+        ReadableByteChannel byteChannel = Channels.newChannel(url.openStream());
+        FileOutputStream stream = new FileOutputStream(file);
+        stream.getChannel().transferFrom(byteChannel, 0, Long.MAX_VALUE);
+        stream.close();
+        byteChannel.close();
+    }
+}
+
