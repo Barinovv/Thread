97f7cbea59236a9ab9078fa60ea7a306e0415119
Barinovv <p.toxa@bk.ru>
2017-12-23 09:12:08 +0300

    Изменены названия потоков.

--- src/ru/bav/copingfiles/Main.java
+++ src/ru/bav/copingfiles/Main.java
@@ -12,13 +12,22 @@ public class Main {
                 "C:\\Users\\User\\IdeaProjects\\Thread\\src\\ru\\bav\\copingfiles\\result file.txt");
         firstCopy.start();
 
+        if (firstCopy.isAlive()) {
+            firstCopy.join();
+        }
         System.out.println("Вставка в первый текстовик занимает - " + (System.currentTimeMillis() - first) + " милесекунд(ы)");
         long second = System.currentTimeMillis();
         Copy secondCopy = new Copy("C:\\Users\\User\\IdeaProjects\\Thread\\src\\ru\\bav\\copingfiles\\main file.txt",
                 "C:\\Users\\User\\IdeaProjects\\Thread\\src\\ru\\bav\\copingfiles\\second result file.txt");
 
+
         secondCopy.start();
 
+
+        if (secondCopy.isAlive()) {
+            secondCopy.join();
+
+        }
         System.out.println("Вставка во второй текстовик занимает  - " + (System.currentTimeMillis() - second) + " милесекунд(ы)");
     }
 }
